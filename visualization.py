77571e69fe32265555f4436e40dbb67f50432e64
SDineshkumar-developer <sdineshkumar0715@gmail.com>
2026-06-02 19:56:53 +0530

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -15,3 +15,12 @@ plt.grid(True)
 plt.savefig("sales_chart.png")
 
 print("Chart saved successfully!")
+plt.figure(figsize=(8,5))
+plt.bar(df["Month"], df["Sales"])
+plt.title("Monthly Sales Bar Chart")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+
+plt.savefig("sales_bar_chart.png")
+
+print("Bar chart saved successfully!")
